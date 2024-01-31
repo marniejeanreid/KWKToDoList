ae937f01c939d74552c64dadd665084812d5aa0e
Marnie Reid <marnie@kodewithklossy.com>
2024-01-31 12:10:15 -0800

    Initial Commit

---
+++ KWKToDoList/KWKToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWKToDoListApp.swift
+//  KWKToDoList
+//
+//  Created by scholar on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct KWKToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
